d310442d11bf30b9df19dc1620a2a94503bb2605
Yogesh Kurmi <kurmiyogi625@gmail.com>
2021-07-16 19:20:40 +0530

    intial-setup

---
+++ src/main/java/com/cryptox/CryptoxApplication.java
@@ -0,0 +1,13 @@
+git remote add origin git@github.com:10yogi/cryptox.gitpackage com.cryptox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptox/CryptoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
